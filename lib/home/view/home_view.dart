d054f2ae5f2edef77d8b4a60ee7f16b6a0558504
ramanverma2k <ramanverma4183@gmail.com>
2022-05-10 18:48:54 +0530

    home_view: integrate bloc with ui

--- lib/home/view/home_view.dart
+++ lib/home/view/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:graphql_spacex_bloc_example/home/bloc/home_bloc.dart';
 
 @immutable
 class HomeView extends StatelessWidget {
@@ -6,6 +8,45 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('GraphQL Bloc Demo'),
+        centerTitle: true,
+      ),
+      body: BlocBuilder<HomeBloc, HomeState>(
+        builder: (context, state) {
+          switch (state.status) {
+            case HomeStatus.initial:
+              return const Center(
+                child: CircularProgressIndicator.adaptive(),
+              );
+            case HomeStatus.loaded:
+              return Column(
+                children: [
+                  Expanded(
+                    child: ListView.separated(
+                      itemBuilder: (_, index) => ListTile(
+                        title: Text(
+                          '${state.rockets[index]?.name}',
+                        ),
+                        subtitle: Text(
+                          '${state.rockets[index]?.description}',
+                        ),
+                      ),
+                      separatorBuilder: (_, index) =>
+                          const SizedBox(height: 10),
+                      itemCount: state.rockets.length,
+                    ),
+                  )
+                ],
+              );
+            case HomeStatus.error:
+              return const Center(
+                child: Text('Error fetching data from graphql endpoint'),
+              );
+          }
+        },
+      ),
+    );
   }
 }
